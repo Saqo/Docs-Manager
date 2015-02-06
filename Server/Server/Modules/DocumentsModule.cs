446e9362733666ebcb19e0b6669d1583f7fff299
Saqo <saqoagadzhanyan@gmail.com>
2015-02-07 00:47:33 +0400

    Create architecture of app. (Main modules  and views in folders)

---
+++ Server/Server/Modules/DocumentsModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Nancy.Security;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Modules
+{
+    class DocumentsModule : Nancy.NancyModule
+    {
+        public DocumentsModule()
+        {
+            this.RequiresAuthentication();
+            Get["/"] = param =>
+            {
+                return View["Views/Documents/Index.html"];
+            };
+        }
+    }
+}
---
+++ Server/Server/Modules/LoginModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class LoginModule : Nancy.NancyModule
+    {
+        public LoginModule()
+            : base("/Login")
+        {
+            Get["/"] = param =>
+            {
+                return View["Views/Login/Form.html"];
+            };
+        }
+    }
+}
