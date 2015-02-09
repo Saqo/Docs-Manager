ab8c8c34373c45e775a7077f8c3f2108b623072c
Saqo <saqoagadzhanyan@gmail.com>
2015-02-09 05:05:57 +0400

    Update Contexts

--- Server/Server/Contexts/DocumentsContext.cs
+++ Server/Server/Contexts/DocumentsContext.cs
@@ -11,21 +11,21 @@ namespace Server.Contexts
 {
     class DocumentsContext
     {
-        private PetaPoco.Database DB = GetDatabase();
-        public Document GetAllByUserId(Guid id)
+        private PetaPoco.Database DB = PetaPocoDBManager.GetDatabase();
+        public IEnumerable<Document> GetAllByUserId(Guid id)
         {
-
-            String sql = "select * from Documents where Id =" + id.ToString();
-            return GetDatabase().FirstOrDefault<Document>(sql);
+            String sql = "select * from Documents where Id ='" + id + "'";
+            return DB.Query<Document>(sql);
         }
-        private static PetaPoco.Database GetDatabase()
+
+        public void Add(Document doc)
         {
-            // A sqlite database is just a file.
-            String fileName = Path.Combine(Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments), "documents.db");
-            String connectionString = "Data Source=" + fileName;
-            PetaPoco.Database db = new PetaPoco.Database(connectionString);
-            return db;
-           
+            DB.Insert(doc);
+        }
+
+        public void Delete(Int64 id)
+        {
+            DB.Delete<Document>(id);
         }
     }
 }
---
+++ Server/Server/Contexts/PetaPocoDBManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Contexts
+{
+    class PetaPocoDBManager
+    {
+        public static PetaPoco.Database GetDatabase()
+        {
+            String connectionString = @"Server=.\SQLExpress;Integrated security=SSPI;Initial Catalog=" + Program.DBFileName;
+            PetaPoco.Database db = new PetaPoco.Database(connectionString, "System.Data.SqlClient");
+            return db;
+        }
+    }
+}
