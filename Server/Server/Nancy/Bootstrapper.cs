0a9880e470823cc8cfed2205abb1991dff5b3b1e
Saqo <saqoagadzhanyan@gmail.com>
2015-02-09 06:24:52 +0400

    Add Bootstrapper

---
+++ Server/Server/Nancy/Bootstrapper.cs
@@ -0,0 +1,44 @@
+﻿using Nancy;
+using Nancy.Authentication.Forms;
+using Nancy.Bootstrapper;
+using Nancy.Conventions;
+using Nancy.TinyIoc;
+using Server.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Nancy
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureRequestContainer(TinyIoCContainer container, NancyContext context)
+        {
+            base.ConfigureRequestContainer(container, context);
+            container.Register<IUserMapper, UsersContext>();
+        }
+
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            base.RequestStartup(container, pipelines, context);
+
+            var formsAuthConfiguration = new FormsAuthenticationConfiguration
+            {
+                RedirectUrl = "~/Login",
+                UserMapper = container.Resolve<IUserMapper>(),
+            };
+
+            FormsAuthentication.Enable(pipelines, formsAuthConfiguration);
+        }
+
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+            nancyConventions.StaticContentsConventions.Clear();
+            //nancyConventions.StaticContentsConventions.Add(
+            //StaticContentConventionBuilder.AddDirectory("/Scripts", "/Content", new[] { ".js" }));
+        }
+    }
+}
